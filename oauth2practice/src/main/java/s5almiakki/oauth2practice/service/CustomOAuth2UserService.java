49dac0c0b228984e2c7ad47ae3e2ca7f9d908a79
5almiakki <jpui9635@gmail.com>
2025-07-17 17:19:07 +0900

    Implement persistence logic for user entity

--- oauth2practice/src/main/java/s5almiakki/oauth2practice/service/CustomOAuth2UserService.java
+++ oauth2practice/src/main/java/s5almiakki/oauth2practice/service/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package s5almiakki.oauth2practice.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -8,9 +9,16 @@ import org.springframework.stereotype.Service;
 import s5almiakki.oauth2practice.dto.CustomOAuth2User;
 import s5almiakki.oauth2practice.dto.NaverOAuth2Response;
 import s5almiakki.oauth2practice.dto.OAuth2Response;
+import s5almiakki.oauth2practice.entity.UserEntity;
+import s5almiakki.oauth2practice.repository.UserRepository;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -24,7 +32,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             default:
                 return null;
         }
-        String role = "ROLE_USER";
+        String username = response.getProvider() + "_" + response.getProviderId();
+        String role = null;
+        Optional<UserEntity> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            role = user.get().getRole();
+        } else {
+            userRepository.save(new UserEntity(username, role));
+        }
         return new CustomOAuth2User(response, role);
     }
 }
