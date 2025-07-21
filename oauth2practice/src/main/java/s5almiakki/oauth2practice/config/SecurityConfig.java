bd530b8f9f3af67d14010d75d1f1f5ba831efc4c
5almiakki <jpui9635@gmail.com>
2025-07-21 11:22:49 +0900

    Add custom service and repository

--- oauth2practice/src/main/java/s5almiakki/oauth2practice/config/SecurityConfig.java
+++ oauth2practice/src/main/java/s5almiakki/oauth2practice/config/SecurityConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import s5almiakki.oauth2practice.oauth2.CustomClientRegistrationRepository;
 import s5almiakki.oauth2practice.service.CustomOAuth2UserService;
 
 @RequiredArgsConstructor
@@ -15,7 +16,9 @@ import s5almiakki.oauth2practice.service.CustomOAuth2UserService;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final CustomClientRegistrationRepository customClientRegistrationRepository;
+//    private final CustomClientRegistrationRepository customClientRegistrationRepository;
+    private final ClientRegistrationRepository  clientRegistrationRepository;
+    private final OAuth2AuthorizedClientService  oAuth2AuthorizedClientService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,7 +29,8 @@ public class SecurityConfig {
                 // oauth2Login은 자동으로 여러가지 설정해주는 반면, oauth2Client는 커스텀할 것들이 많다.
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
-                        .clientRegistrationRepository(customClientRegistrationRepository.clientRegistrationRepository())
+                        .clientRegistrationRepository(clientRegistrationRepository)
+                        .authorizedClientService(oAuth2AuthorizedClientService)
                         .userInfoEndpoint(config -> config
                                 .userService(customOAuth2UserService)))
                 .authorizeHttpRequests(request -> request
--- oauth2practice/src/main/java/s5almiakki/oauth2practice/oauth2/CustomClientRegistrationRepository.java
+++ oauth2practice/src/main/java/s5almiakki/oauth2practice/oauth2/CustomClientRegistrationRepository.java
@@ -1,6 +1,7 @@
 package s5almiakki.oauth2practice.oauth2;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -11,6 +12,7 @@ public class CustomClientRegistrationRepository {
 
     private final CustomClientRegistration customClientRegistration;
 
+    @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
         return new InMemoryClientRegistrationRepository(
                 customClientRegistration.naverClientRegistration());
